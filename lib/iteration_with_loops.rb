22a86f0929d18009837dc12863b67e750ef9c9ce
Davis Brown <cooliomoded@gmail.com>
2020-08-11 20:38:24 +0000

    Automatically backed up by Learn

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -4,7 +4,7 @@ def join_nested_strings(src)
   while i < src.count do
     j = 0
     while j < src[i][j].count do
-      if (src[i][j].class === String) == true
+      if src[i][j].class == 'String'
       joined_strings += src[i][j]
       end
       j += 1
